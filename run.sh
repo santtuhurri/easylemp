7afcc0fc2847be94219f1979ab90ee1313bb9dc1
santtuhurri <103632166+santtuhurri@users.noreply.github.com>
2022-05-17 15:25:09 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ echo "Retrieving and Applying Settings..."
 git clone https://github.com/santtuhurri/easylemp.git
 cd easylemp/
 
-sudo cp -R nginx /srv/salt
-sudo cp -R mariadb /srv/salt
-sudo cp -R php /srv/salt
+sudo cp -R nginx /srv/salt/
+sudo cp -R mariadb /srv/salt/
+sudo cp -R php /srv/salt/
 sudo cp top.sls /srv/salt/top.sls
 
 echo "You can now try the state with 'sudo salt-call --local state.apply'"
